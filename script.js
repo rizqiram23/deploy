bf3522abbb63d6c610d194df5db503a442d2d5be
rizqiram23 <70790829+rizqiram23@users.noreply.github.com>
2020-10-17 08:54:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// sheep audio
+document.getElementById('sheep').addEventListener('click',function (e) {
+    e.preventDefault();
+    document.getElementById('audio').play();
+});
+
+
+// cow audio
+document.getElementById('cow').addEventListener('click',function (e) {
+    e.preventDefault();
+    document.getElementById('cow_audio').play();
+});
+
+// duck audio
+document.getElementById('duck').addEventListener('click',function (e) {
+    e.preventDefault();
+    document.getElementById('duck_audio').play();
+});
+
+// lion audio
+document.getElementById('lion').addEventListener('click',function (e) {
+    e.preventDefault();
+    document.getElementById('lion_audio').play();
+});
